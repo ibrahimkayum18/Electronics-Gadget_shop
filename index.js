e97f6659d2e890e07960b95fd764213c9da17099
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2023-10-23 09:45:31 -0700

    All cart added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(express.json());
 //D8w95RdmzdUl9jrJ
 
 
-const uri = `mongodb+srv:// ibrahimkayum017:D8w95RdmzdUl9jrJ@cluster0.pqcfxjd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://ibrahimkayum017:D8w95RdmzdUl9jrJ@cluster0.pqcfxjd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,11 +29,23 @@ async function run() {
     await client.connect();
 
     const shopifyCollection = client.db("shopifyProductsDB").collection("shopifyProducts")
-    //Access data from front end
+    const cartCollection = client.db("shopifyProductsDB").collection("cart")
+    //Access data from front end into shopify product
     app.post('/shopifyProducts', async (req, res) => {
         const products = req.body;
         const result = await shopifyCollection.insertOne(products);
         res.send(result)
+    })
+    //Access data fom client side into cart collection
+    app.post('/cart', async(req, res) => {
+      const data = req.body;
+      const result = await cartCollection.insertOne(data)
+      res.send(result)
+    })
+
+    app.get('/cart', async(req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
     })
 
     app.get('/shopifyProducts', async(req, res) => {
@@ -41,7 +53,42 @@ async function run() {
           res.send(result);
       })
 
+      app.post('/shopifyProducts/:brand', async(req, res) => {
+        const product = req.params.brand;
+        const result = await appleCollection.insertOne(product)
+        res.send(result)
+      })
 
+      app.get('/shopifyProducts/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {
+          _id: new ObjectId(id)
+        }
+        const result = await shopifyCollection.findOne(filter)
+        res.send(result)
+      })
+
+      app.put('/shopifyProducts/:id', async(req, res) => {
+        const id = req.params.id;
+        const data = req.body;
+        const filter = {
+            _id: new ObjectId(id)
+        };
+        const option = {upsert: true}
+        const updateData = {
+            $set: {
+                name: data.name,
+                brand: data.brand,
+                p_type: data.p_type,
+                price: data.price,
+                rating: data.rating,
+                p_description: data.p_description,
+                photo: data.photo
+            }
+        }
+        const result = await shopifyCollection.updateOne(filter, updateData, option)
+        res.send(result)
+    })
 
 
 
